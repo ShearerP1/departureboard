3db25d95add6cc82bc43c6c518ee6327a6d92ada
Patrick Shearer <127220665+ShearerP1@users.noreply.github.com>
2026-01-23 00:21:48 +0100

    Add setup script for Termux environment
    This script installs necessary packages for the Termux environment, clones a repository, and sets up a Python virtual environment with required dependencies. This avoids typing a lot on a tablet screen.

---
+++ dependencies.sh
@@ -0,0 +1,14 @@
+#some packages required to get things setup on the termux environment on the tablet 
+pkg install openssh
+pkg install git
+pkg update && pkg upgrade
+
+#if needed to grab the most updated version
+git clone https://github.com/ShearerP1/departureboard.git
+cd departureboard
+
+#set up the environment and install required packages
+python -m venv venv
+source venv/bin/activate
+pip install flask requests pythjon-dotenv gunicorn tzdata
+python tt.py
