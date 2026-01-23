101bb07b63166785ac615478c84d8664197ef8a2
Patrick Shearer <127220665+ShearerP1@users.noreply.github.com>
2026-01-23 01:05:03 +0100

    Comment out setup commands in dependencies.sh
    Comment out package installation and git clone commands.

--- dependencies.sh
+++ dependencies.sh
@@ -1,11 +1,11 @@
 #some packages required to get things setup on the termux environment on the tablet 
-pkg install openssh
-pkg install git
-pkg update && pkg upgrade
+#pkg install openssh
+#pkg install git
+#pkg update && pkg upgrade
 
 #if needed to grab the most updated version
-git clone https://github.com/ShearerP1/departureboard.git
-cd departureboard
+#git clone https://github.com/ShearerP1/departureboard.git
+#cd departureboard
 
 #set up the environment and install required packages
 python -m venv venv
